191a067804ea22383afabcea76030243f8a08a5e
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2025-04-18 00:10:33 -0700

    created commands that generated containers and subcontainers based on square numbers

--- javascript.js
+++ javascript.js
@@ -7,18 +7,55 @@
 // function generateBoxes(input) {
 
 // }
+//Generates containers for the main drawing art boundary
+function createContainer() {
+    let columnContainer = document.createElement("div");
+    columnContainer.style.display = 'flex';
+    columnContainer.style.flexDirection = "column";
+    columnContainer.style.backgroundColor = 'white';
+    // columnContainer.style.margin = '10px';
+    columnContainer.style.flexGrow = 1;
 
-let art_box_boundary = document.querySelector(".art_box_boundaries")
-if (art_box_boundary) {
-
+    return(columnContainer)
+}
+//Generated boxes for the containers
+function createBox() {
     const box = document.createElement("div")
     box.style.display = 'flex';
     box.style.backgroundColor = 'black';
     box.style.margin = '5px';
     box.style.flexGrow = 1;
     
-    art_box_boundary.appendChild(box);
+    return(box)
 }
-else{
-    console.error("No entity found");
+//fitting boxes into containers based on number
+function fitBoxes(container, quantity){
+    for (let i = 0; i < quantity; i++){
+        container.appendChild(createBox())
+    }
 }
+
+//Generates a container filled with boxes. Input value correlates with number of boxes in container
+function generateFilledContainer(quantity){
+    container = createContainer()
+    fitBoxes(container, quantity);
+    return (container)
+}
+
+
+//Ignore function for now
+function boxUpdate(masterContainer, input){
+    if (masterContainer) {
+        for (let i = 0; i < input; i++){
+            masterContainer.appendChild(generateFilledContainer(input))
+        }
+    }
+    else{
+        console.error("No entity found");
+    }
+}
+
+let art_box_boundary = document.querySelector(".art_box_boundaries")
+boxUpdate(art_box_boundary, 2)
+
+//Next time improve the margins of the boxes to make them all uniform
