b0cc8d1511727356412203af17fb25f25daa9d13
anthonyk191 <67760299+anthonyk191@users.noreply.github.com>
2025-04-26 19:27:46 -0700

    Added buttons that change paint brush colors, updated class names to be consistent to PascalNaming conventions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,46 @@
+let paintBrush = "black"
+let art_box_boundary = document.querySelector(".ArtBoxBoundaries")
+fillMasterContainer(art_box_boundary, 50)
+
+//Button Section
+document.getElementById('BlackBtn').onclick = () => {
+    paintBrush = "black"
+}
+document.getElementById('WhiteBtn').onclick = () => {
+    paintBrush = "white"
+}
+document.getElementById('RainbowBtn').onclick = () => {
+    paintBrush = "rainbow"
+}
+document.getElementById('ClearEverythingBtn').onclick = () => {
+    fillMasterContainer(art_box_boundary, slider.value)
+}
+//-----------------------------------------------------------------------
+//Mouse Commands: When mouse press down, update square color
+const boxes = document.querySelectorAll('.ArtBoxBoundaries > div > div');
+let isMouseDown = false;
+
+//Suboptimal Code that is slower for registering mouse down and mouse up events
+// document.addEventListener('mousedown', () => {
+//     isMouseDown = true;
+//     console.log("mouse down")
+// })
+// document.addEventListener('mouseup', () => {
+//     isMouseDown = false;
+//     console.log("mouse up")
+// })
+
+art_box_boundary.onmousedown = () => (isMouseDown = true)
+art_box_boundary.onmouseup = () => (isMouseDown = false)
+
+//Updating the slider
+var slider = document.querySelector(".Slider");
+slider.addEventListener('change', () => {
+    fillMasterContainer(art_box_boundary, slider.value)
+})
+
+
+//ALL FUNCTIONS DOWN BELOW
 //Generates containers for the main drawing art boundary
 function createContainer() {
     let columnContainer = document.createElement("div");
@@ -21,11 +64,11 @@ function createBox() {
     //Setting up event listeners per box
     box.addEventListener("mouseenter", () => {
         if (isMouseDown) {
-            box.style.backgroundColor = randomColor()
+            box.style.backgroundColor = paint(paintBrush)
         }
     })
     box.addEventListener("mousedown", () => {
-        box.style.backgroundColor = randomColor()})
+        box.style.backgroundColor = paint(paintBrush)})
     return(box);
 }
 
@@ -46,7 +89,7 @@ function generateSubContainer(quantity){
 
 //Master Function for generating all boxes on the drawing grid
 function fillMasterContainer(masterContainer, input){
-    
+    masterContainer.innerHTML = '';
     //Limits how many drawing spaces can be made to avoid user overloading program
     if (input > 100) {
         input = 100;
@@ -75,25 +118,26 @@ function randomColor(){
     return(color)
 }
 
-let art_box_boundary = document.querySelector(".art_box_boundaries")
-fillMasterContainer(art_box_boundary, 4)
+//Generate black color function
+function blackColor() {
+    return("#000000")
+}
 
-//-----------------------------------------------------------------------
-//Mouse Commands: When mouse press down, update square color
-const boxes = document.querySelectorAll('.art_box_boundaries > div > div');
-let isMouseDown = false;
+//Generate white color function
+function whiteColor() {
+    return("#FFFFFF")
+}
 
-// document.addEventListener('mousedown', () => {
-//     isMouseDown = true;
-//     console.log("mouse down")
-// })
-// document.addEventListener('mouseup', () => {
-//     isMouseDown = false;
-//     console.log("mouse up")
-// })
-
-art_box_boundary.onmousedown = () => (isMouseDown = true)
-art_box_boundary.onmouseup = () => (isMouseDown = false)
-
+//Color Manager - Converts 
+function paint(paintBrush) {
+    switch (paintBrush) {
+        case "black":
+            return(blackColor()) 
+        case "white":
+            return(whiteColor())
+        case "rainbow":
+            return(randomColor())
+    }
+}
 
 //Future plans consider using document.createDocumentFragment() for optimzation
